dcfcb61482012ccc74400569928824fb3226539f
Sharif Uddin <sharifuddin6@outlook.com>
2015-10-11 19:07:05 +0100

    Added files.

---
+++ Project/Controllers/StockController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using Project.Domain.Repositories;
+using Project.Infrastructure.Repositories;
+using Project.ViewModels.Stock;
+
+namespace Project.Controllers
+{
+    public class StockController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        
+        public StockController(IProductRepository productRepository)
+        {
+            this._productRepository = productRepository;
+        }
+        
+        [HttpGet]
+        public ActionResult Stock()
+        {
+            var products = _productRepository.GetAllProducts();
+
+            return View(new StockViewModel
+            {
+                Products = products,
+                Product = new Product()
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Stock(Product product)
+        {
+            _productRepository.AddNewProduct(product);
+            return RedirectToAction("Stock");
+        }
+    }
+}
