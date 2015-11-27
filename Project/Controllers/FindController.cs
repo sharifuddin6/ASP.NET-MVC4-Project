3f1dab70c813836577b57a68df5674ce5225b7a6
Sharif Uddin <sharifuddin6@outlook.com>
2015-11-27 15:14:14 +0000

    Added lost files.

---
+++ Project/Controllers/FindController.cs
@@ -0,0 +1,24 @@
+﻿using Project.Domain.Repositories;
+using System.Web.Mvc;
+using Project.ViewModels.Find;
+
+namespace Project.Controllers
+{
+    public class FindController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public FindController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public ActionResult Find()
+        {
+            return View("Find", new FindViewModel
+            {
+                Products = _productRepository.GetAllProducts()
+            });
+        }
+    }
+}
