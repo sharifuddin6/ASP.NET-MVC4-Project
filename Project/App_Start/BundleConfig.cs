959071c2f826bb601d4b5332b709f036fb0fa3bc
Sharif Uddin <sharifuddin6@outlook.com>
2015-12-22 20:15:37 +0000

    Implemented a looping slideshow of images.

--- Project/App_Start/BundleConfig.cs
+++ Project/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace Project
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-2.1.4.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
---
+++ Project/Controllers/SlideController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class SlideController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Slide");
+        }
+    }
+}
