de710df1cc4ca81eef76e4ad3c3eb74452165542
Sharif Uddin <sharifuddin6@outlook.com>
2015-12-23 19:13:31 +0000

    Added navigation panel to navigate slideshow.

--- Project/Scripts/app/slide.js
+++ Project/Scripts/app/slide.js
@@ -1,24 +1,64 @@
 ﻿
-function init($slide) {
-    var slideCount = $slide.children().length;
-    var currentSlide = 1;
-    var previousSlide = slideCount;
-    var fade = 5000;
+var $slide;
 
-    setInterval(function () {
-        $slide.find("li:eq(" + currentSlide + ")").fadeIn(fade);
-        $slide.find("li:eq(" + previousSlide + ")").fadeOut(fade);
+function init(slide) {
+    $slide = slide;
+    initSlideSelector();
+    var loop = slideLoop(0);
 
-        currentSlide++;
-        previousSlide++;
+    $(".slide-selector input").on("change", function () {
+        clearInterval(loop);
+        slideChange($(this).index());
+        loop = slideLoop($(this).index());
+    });
+}
 
-        if (currentSlide > slideCount) {
-            currentSlide = 1;
-        }
+function initSlideSelector() {
+    var $slideContainer = $slide.parent();
+    var $selectorContainer = $(document.createElement("div"));
+    $selectorContainer.addClass("slide-selector");
 
-        if (previousSlide > slideCount) {
-            previousSlide = 1;
-        }
+    for (var i = 0; i < $slide.children().length; i++) {
+        var $selector = $("<input type='radio' name='slide' />");
+        $selector.appendTo($selectorContainer);
+    }
 
-    }, fade * 2);
+    $slideContainer.append($selectorContainer);
+}
+
+function slideLoop(currentSlide) {
+    var slideIndex = currentSlide;
+    var fadeDuration = 5000;
+    $(".slide-selector input:eq(" + slideIndex + ")").prop("checked", true);
+
+    return setInterval(function () {
+        slideIndex = nextSlideIndex(slideIndex);
+        $(".slide-selector input:eq(" + slideIndex + ")").prop("checked", true);
+        $slide.find("li").fadeOut(fadeDuration);
+        $slide.find("li:eq(" + slideIndex  + ")").fadeIn(fadeDuration);
+    }, fadeDuration * 2);
+}
+
+function slideChange(currentSlide) {
+    var fadeDuration = 1000;
+    $slide.find("li").fadeOut(fadeDuration);
+    $slide.find("li:eq(" + currentSlide + ")").fadeIn(fadeDuration);
+}
+
+function nextSlideIndex(currentSlide) {
+    currentSlide++;
+    if (currentSlide > $slide.children().length - 1) {
+        currentSlide = 0;
+    }
+
+    return currentSlide;
+}
+
+function previousSlideIndex(currentSlide) {
+    currentSlide--;
+    if (currentSlide < 0) {
+        currentSlide = $slide.children().length - 1;
+    }
+
+    return currentSlide;
 }
