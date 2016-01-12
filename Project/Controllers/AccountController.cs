6be859dad13f6f5776358ce39429536443082965
Sharif Uddin <sharifuddin6@outlook.com>
2016-01-12 13:59:43 +0000

    Removed unused code and views.

--- Project/Controllers/AccountController.cs
+++ Project/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -58,23 +59,6 @@ namespace Project.Controllers
             }
 
             return View(model);
-
-            //// This doesn't count login failures towards account lockout
-            //// To enable password failures to trigger account lockout, change to shouldLockout: true
-            //var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
-            //switch (result)
-            //{
-            //    case SignInStatus.Success:
-            //        return RedirectToLocal(returnUrl);
-            //    case SignInStatus.LockedOut:
-            //        return View("Lockout");
-            //    case SignInStatus.RequiresVerification:
-            //        return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
-            //    case SignInStatus.Failure:
-            //    default:
-            //        ModelState.AddModelError("", "Invalid login attempt.");
-            //        return View(model);
-            //}
         }
 
         //
@@ -105,35 +89,14 @@ namespace Project.Controllers
             }
 
             return View(model);
-            
-            //var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-            //var result = await UserManager.CreateAsync(user, model.Password);
-            //if (result.Succeeded)
-            //{
-            //    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-
-            //     For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-            //     Send an email with this link
-            //     string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-            //     var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-            //     await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-            //    return RedirectToAction("Index", "Home");
-            //}
-            //AddErrors(result);
         }
 
         [Authorize(Roles = "Admin")]
         public ActionResult Index()
         {
-            var Db = new ApplicationDbContext();
-            var users = Db.Users;
-            var model = new List<EditUserViewModel>();
-            foreach (var user in users)
-            {
-                var u = new EditUserViewModel(user);
-                model.Add(u);
-            }
+            var db = new ApplicationDbContext();
+            var users = db.Users;
+            var model = users.Select(user => new EditUserViewModel(user)).ToList();
             return View(model);
         }
 
