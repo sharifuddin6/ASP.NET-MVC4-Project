cb5c5e28bef08540f09787ea47bdc6f75370287b
Sharif Uddin <sharifuddin6@outlook.com>
2016-01-21 18:38:35 +0000

    Removed dangerous global variable in javascript.

--- Project/Scripts/app/slide.js
+++ Project/Scripts/app/slide.js
@@ -1,19 +1,16 @@
 ﻿
-var $slide;
-
-function init(slide) {
-    $slide = slide;
-    initSlideSelector();
-    var loop = slideLoop(0);
+function init($slide) {
+    initSlideSelector($slide);
+    var loop = slideLoop($slide, 0);
 
     $(".slide-selector input").on("change", function () {
         clearInterval(loop);
-        slideChange($(this).index());
-        loop = slideLoop($(this).index());
+        slideChange($slide, $(this).index());
+        loop = slideLoop($slide, $(this).index());
     });
 }
 
-function initSlideSelector() {
+function initSlideSelector($slide) {
     var $slideContainer = $slide.parent();
     var $selectorContainer = $(document.createElement("div"));
     $selectorContainer.addClass("slide-selector");
@@ -26,38 +23,29 @@ function initSlideSelector() {
     $slideContainer.append($selectorContainer);
 }
 
-function slideLoop(currentSlide) {
+function slideLoop($slide, currentSlide) {
     var slideIndex = currentSlide;
     var fadeDuration = 5000;
     $(".slide-selector input:eq(" + slideIndex + ")").prop("checked", true);
 
     return setInterval(function () {
-        slideIndex = nextSlideIndex(slideIndex);
+        slideIndex = nextSlideIndex($slide, slideIndex);
         $(".slide-selector input:eq(" + slideIndex + ")").prop("checked", true);
         $slide.find("li").fadeOut(fadeDuration);
         $slide.find("li:eq(" + slideIndex  + ")").fadeIn(fadeDuration);
     }, fadeDuration * 2);
 }
 
-function slideChange(currentSlide) {
+function slideChange($slide, currentSlide) {
     var fadeDuration = 1000;
     $slide.find("li").fadeOut(fadeDuration);
     $slide.find("li:eq(" + currentSlide + ")").fadeIn(fadeDuration);
 }
 
-function nextSlideIndex(currentSlide) {
+function nextSlideIndex($slide, currentSlide) {
     currentSlide++;
     if (currentSlide > $slide.children().length - 1) {
         currentSlide = 0;
-    }
-
-    return currentSlide;
-}
-
-function previousSlideIndex(currentSlide) {
-    currentSlide--;
-    if (currentSlide < 0) {
-        currentSlide = $slide.children().length - 1;
     }
 
     return currentSlide;
